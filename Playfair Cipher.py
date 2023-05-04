a46965f059d8437ccb2f60673c8c0d5743788978
20CSESumanth <112487116+20CSE452-Sumanth@users.noreply.github.com>
2023-05-04 12:17:07 +0530

    LAB

--- Playfair Cipher.py
+++ Playfair Cipher.py
@@ -1,88 +1,49 @@
-# Create a 5x5 matrix using a secret key
-def create_matrix(key):
-    key = key.upper()
-    matrix = [[0 for i in range(5)] for j in range(5)]
-    letters_added = []
-    row = 0
-    col = 0
-    # add the key to the matrix
-    for letter in key:
-        if letter not in letters_added:
-            matrix[row][col] = letter
-            letters_added.append(letter)
-        else:
-            continue
-        if (col == 4):
-            col = 0
-            row += 1
-        else:
-            col += 1
-    # Add the rest of the alphabet to the matrix
-    # A=65 ... Z=90
-    for letter in range(65, 91):
-        if letter == 74:  # I/J are in the same position
-            continue
-        if chr(letter) not in letters_added:  # Do not add repeated letters
-            letters_added.append(chr(letter))
+import re
 
-    # print (len(letters_added), letters_added)
-    index = 0
-    for i in range(5):
-        for j in range(5):
-            matrix[i][j] = letters_added[index]
-            index += 1
+# Function to generate the Playfair matrix
+def generate_matrix(key):
+    key = key.upper().replace("J", "I") # Convert to uppercase and replace J with I
+    key = re.sub(r"[^A-Z]", "", key)   # Remove non-alphabetic characters
+    key += "ABCDEFGHIKLMNOPQRSTUVWXYZ" # Append the remaining alphabets
+    
+    # Create the Playfair matrix
+    matrix = [key[i:i+5] for i in range(0, 25, 5)]
     return matrix
-# Add fillers if the same letter is in a pair
-def separate_same_letters(message):
-    index = 0
-    while (index < len(message)):
-        l1 = message[index]
-        if index == len(message)-1:
-            message = message + 'X'
-            index += 2
-            continue
-        l2 = message[index+1]
-        if l1 == l2:
-            message = message[:index+1] + "X" + message[index+1:]
-            index += 2
-            return message
 
+# Function to encrypt a message using the Playfair matrix
+def encrypt(message, matrix):
+    message = message.upper().replace("J", "I") # Convert to uppercase and replace J with I
+    message = re.sub(r"[^A-Z]", "", message)   # Remove non-alphabetic characters
+    
+    # Insert "X" between repeated letters and add "Z" at the end if the length is odd
+    message = re.sub(r"(.)\1", r"\1X\1", message)
+    message = message if len(message) % 2 == 0 else message + "Z"
+    
+    # Encrypt the message using the Playfair cipher
+    ciphertext = ""
+    for i in range(0, len(message), 2):
+        a, b = message[i], message[i+1]
+        row1,col1 = divmod(matrix.index(a),5)
+        row2,col2 = divmod(matrix.index(b),5)
+        
+        if row1 == row2:
+            ciphertext += matrix[row1][(col1+1)%5] + matrix[row2][(col2+1)%5]
+        elif col1 == col2:
+            ciphertext += matrix[(row1+1)%5][col1] + matrix[(row2+1)%5][col2]
+        else:
+            ciphertext += matrix[row1][col2] + matrix[row2][col1]
+    
+    return ciphertext
 
-# Return the index of a letter in the matrix
-# This will be used to know what rule (1-4) to apply
+# Test the functions with the given Playfair matrix and message
+matrix = generate_matrix("MFHIJKUNOPQZVWXYELARGDSTBC")
+message = "Must see you over Cadogan West. Coming at once."
+ciphertext = encrypt(message,matrix)
 
+print("Playfair matrix:")
+for row in matrix:
+    print(row)
 
-def indexOf(letter, matrix):
-    for i in range(5):
-        try:
-            index = matrix[i].index(letter)
-            return (i, index)
-        except:
-            continue
-# Implementation of the playfair cipher
-# If encrypt=True the method will encrypt the message
-# otherwise the method will decrypt
-def playfair(key, message, encrypt=True):
-    inc = 1
-    if encrypt == False:
-        inc = -1
-        matrix = create_matrix(key)
-        message = message.upper()
-        message = message.replace(' ', '')
-        message = separate_same_letters(message)
-        cipher_text = ' '
-        for (l1, l2) in zip(message[0::2], message[1::2]):
-            row1, col1 = indexOf(l1, matrix)
-            row2, col2 = indexOf(l2, matrix)
-            if row1 == row2:  # Rule 2, the letters are in the same row
-                cipher_text += matrix[row1][(col1+inc) % 5] + matrix[row2][(col2+inc) % 5]
-            elif(col1 == col2):  # Rule 3, the letters are in the same column
-                ipher_text += matrix[(row1+inc) % 5][col1] + matrix[(row2+inc) % 5][col2]
-            else:  # Rule 4, the letters are in a different row and column
-                cipher_text += matrix[row1][col2] + matrix[row2][col1]
-                return cipher_text
-if __name__ == '__main__':
-    print('Encripting')
-    print(playfair('secret', 'my secret message'))
-    print('Decrypting')
-    print(playfair('secret', 'LZECRTCSITCVAHBT', False))
+print("\nPlaintext: ", message)
+print("Ciphertext:", ciphertext)
+
